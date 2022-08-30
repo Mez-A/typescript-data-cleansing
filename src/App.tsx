0036b92fe3941f0969921dc2873d9a8217eeee85
Mez-A <dimoabr@gmail.com>
2022-08-30 10:11:54 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>TypeScript site example</h1>
+      <p>Testing</p>
+    </div>
+  )
+}
+
+export default App
