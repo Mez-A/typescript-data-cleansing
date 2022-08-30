721f9d0656274ebe9184d03e45f74168953c2533
Mez-A <dimoabr@gmail.com>
2022-08-30 11:44:29 +0200

    fetching api with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.scss'
+import axios from 'axios'
+
+const url = 'https://edwardtanguay.netlify.app/share/techBooksUnstructured.json'
 
 function App() {
+  const [books, setBooks] = useState([])
+
+   useEffect(() => {
+    (async () => {
+      setBooks((await axios.get(url)).data)
+    })();
+  }, []);
 
   return (
     <div className="App">
       <h1>TypeScript site example</h1>
       <p>Testing</p>
+      <h2>There are {books.length} books.</h2>
     </div>
   )
 }
